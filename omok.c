d5220269b1fc77a39851483600df19a7edca18a1
JH9892 <jhchoi09@naver.com>
2021-01-28 20:10:27 +0900

    checking keyboarding part

--- omok.c
+++ omok.c
@@ -14,7 +14,7 @@ void show_stone(){
     // 바둑돌을 보여주는 함수
 };
 
-void move_position(char key, int* x1, int* y1, int x_b, int y_b){
+void move_position(char key, int* x1, int* y1, int x_b, int y_b) {
     // 사용자가 입력하는 화살표 키에 따라 좌표 x, y값을 변경하는 함수
     switch (key) {
         case 72:// 상 방향키 
@@ -22,11 +22,11 @@ void move_position(char key, int* x1, int* y1, int x_b, int y_b){
             if (*y1 < 1)   *y1 = 1;
             break;
         case 75:// 좌 방향키
-            *x1 = *x1 - 2;    // 좌측으로 2씩 감소(확장형 코드라 2byte씩 이동)
+            *x1 = *x1 - 2;    // 좌측으로 2씩 감소
             if (*x1 < 1)   *x1 = 1; // x축의 최대값
             break;
         case 77:// 우 방향키
-            *x1 = *x1 + 2; //오른쪽 방향의 화살표 키 입력, 우측으로 2씩 증가(확장형 코드라 2byte씩 이동)
+            *x1 = *x1 + 2; //오른쪽 방향의 화살표 키 입력, 우측으로 2씩 증가
             if (*x1 > x_b)  *x1 = x_b;
             break;
         case 80:// 하 방향키
