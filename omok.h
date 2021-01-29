222ddd1087ee8be4c7fbd77c9736a88f7ad1a724
sangyeop910 <75114930+sangyeop910@users.noreply.github.com>
2021-01-29 23:45:01 +0900

    Update omok.h

--- omok.h
+++ omok.h
@@ -27,63 +27,10 @@
 
 // Custom Function Declaration
 void game_control(void);
-void gotoxy(int x, int y);
+void gotoxy();
 void show_stone();
 void move_position();
 void show_map();
 // etc...
-
-void gotoxy(int x, int y){//좌표설정을 위 함 
-	COORD Pos = { x * 2,y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
-}
-
-void show_map(){//바둑판 
-	gotoxy(0, 0);
-	int i, j = 0;
-	for(i = 0; i < MAX_Scale; i++){
-		for (j = 0; j < MAX_Scale; j++) {
-			gotoxy(j, i);
-			if(i == 0){
-				if(j == 0)
-					printf("┌");
-				else if(j == MAX_Scale - 1)
-					printf("┐");
-				else
-					printf("┬"); 
-			}
-			else if(j == 0){
-				if(i == MAX_Scale - 1)
-					printf("└");
-				else
-					printf("├");
-			}
-			else if(j == MAX_Scale - 1){
-				if(i == MAX_Scale - 1)
-					printf("┘");
-				else
-					printf("┤");
-			}
-			else if(i == MAX_Scale - 1){
-				printf("┴");
-			}
-			else
-				printf("┼"); 
-		}
-		printf("\n");
-	}
-	for (i = 0; i < MAX_Scale; i++) {
-		gotoxy(MAX_Scale, i);
-		printf("%d", i+1);
-		gotoxy(i, MAX_Scale);
-		printf("%d", i+1);
-		}
-	}
-
-int main(){
-	show_map();
-	
-	return 0;
-}
 
 # endif
